79b83da369df3a4c6a06ece2c4c873921fd544f0
gh <719636301@qq.com>
2017-10-14 10:34:15 +0800

    ratings1

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,19 +1,70 @@
 <template>
- <div>
-   i am ratings
- </div>
-  
+  <div class="ratings">
+    <div class="ratings-wrapper">
+      <div class="overview">
+        <div class="overview-left">
+          <div class="score">{{seller.score}}</div>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right"></div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-export default {
-}
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  }
 </script>
 
 
 <style lang="scss">
- 
-    
+  .ratings {
+    position: absolute;
+    top: 174px;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    .overview {
+      display: flex;
+      .overview-left {
+        flex: 0 0 137px;
+        width: 137px;
+        padding: 18px 0;
+        text-align: center;
+        .score {
+          margin-bottom: 6px;
+          line-height: 28px;
+          font-size: 24px;
+          color: rgb(255, 153, 0);
+        }
+        .title {
+          margin-bottom: 8px;
+          line-height: 12px;
+          font-size: 12px;
+          color: rgb(7, 17, 27);
+        }
+        .rank {
+          margin-bottom: 6px;
+          line-height: 10px;
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+      }
+      .overview-right {
+        flex: 1;
+      }
+    }
 
-  
+  }
+
+
 </style>
